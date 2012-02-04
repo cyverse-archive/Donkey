159d35b5cfea10ed6269cc4029540800f6d3812c
Dennis Roberts <dennis@iplantcollaborative.org>
2012-02-03 17:10:57 -0700

    CORE-2850: begin implementing the standalone service

--- project.clj
+++ project.clj
@@ -1,3 +1,14 @@
 (defproject Donkey "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "Framework for hosting DiscoveryEnvironment metadata services."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [compojure "1.0.1"]
+                 [swank-clojure "1.4.0-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
+                 [ring/ring-jetty-adapter "1.0.1"]]
+  :dev-dependencies [[lein-ring "0.4.5"]
+                     [swank-clojure "1.2.1"]]
+  :aot [donkey.core]
+  :main donkey.core
+  :repositories {"iplantCollaborative"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
