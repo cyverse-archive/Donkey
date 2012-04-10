0f357f2b2e879bca6b8dcdedc597b9c55703e4c0
Dennis Roberts <dennis@iplantcollaborative.org>
2012-04-09 17:23:02 -0700

    begin modifying this project so that it uses lein-iplant-rpm

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject donkey "1.0.0-SNAPSHOT"
+(defproject donkey "1.1.0-SNAPSHOT"
   :description "Framework for hosting DiscoveryEnvironment metadata services."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
@@ -14,12 +14,19 @@
                  [org.slf4j/slf4j-api "1.5.8"]
                  [org.slf4j/slf4j-log4j12 "1.5.8"]
                  [net.sf.json-lib/json-lib "2.4" :classifier "jdk15"]]
-  :dev-dependencies [[lein-ring "0.4.5"]
+  :dev-dependencies [[org.iplantc/lein-iplant-rpm "1.0.0-SNAPSHOT"]
+                     [lein-ring "0.4.5"]
                      [swank-clojure "1.4.0-SNAPSHOT"]]
   :extra-classpath-dirs ["conf/test"]
   :aot [donkey.core]
   :main donkey.core
   :ring {:handler donkey.core/app :init donkey.core/load-configuration}
+  :iplant-rpm {:summary "iPlant Discovery Environment Metadata Services"
+               :release 1
+               :provides "donkey"
+               :dependencies ["iplant-service-config >= 0.1.0-4"]
+               :config-files ["log4j.properties" "reference_genomes.json"]
+               :config-path "conf/main"}
   :uberjar-exclusions [#"BCKEY.SF"]
   :repositories {"iplantCollaborative"
                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
