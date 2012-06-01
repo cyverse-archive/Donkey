be2a3318cbd4ad52f1b3ae05b5cab792cd09a09d
Dennis Roberts <dennis@iplantcollaborative.org>
2012-06-01 15:46:29 -0700

    CORE-3219: fixed a bug in the session deletion service

--- src/donkey/user_sessions.clj
+++ src/donkey/user_sessions.clj
@@ -2,18 +2,25 @@
   (:use [slingshot.slingshot :only [try+ throw+]]
         [clojure-commons.error-codes]
         [donkey.config]
+        [donkey.service]
         [donkey.user-attributes])
   (:require [clj-http.client :as cl]
             [clojure.string :as string]
             [clojure.tools.logging :as log]
             [clojure-commons.file-utils :as ft]))
 
-(defn key-url
+(defn- key-url
   [user]
   (str 
     (string/join "/" 
       (map ft/rm-last-slash [(riak-base-url) (riak-sessions-bucket) user]))
     "?returnbody=true"))
+
+(defn- request-failed
+  "Throws an exception for a failed request."
+  [resp]
+  (throw+ {:error_code ERR_REQUEST_FAILED
+           :body       (:body resp)}))
 
 (defn user-session
   ([]
@@ -23,8 +30,7 @@
         (cond
           (= 200 (:status resp)) (:body resp)
           (= 404 (:status resp)) "{}"
-          :else (throw+ {:error_code ERR_REQUEST_FAILED
-                         :body (:body resp)})))))
+          :else                  (request-failed resp)))))
 
   ([new-session]
     (let [user (:username current-user)]
@@ -35,17 +41,16 @@
                    {:throw-exceptions false})]
         (cond
           (= 200 (:status resp)) (:body resp)
-          :else                  (throw+ {:error_code ERR_REQUEST_FAILED 
-                                          :body (:body resp)}))))))
+          :else                  (request-failed resp))))))
 
 (defn remove-session
   "Removes user session information from the Riak cluster."
   []
-  (let [user (:username current-user)
-        url  (key-url user)
-        _    (log/debug "user-session: DELETE" url)
-        resp (cl/delete url {:throw-exceptions false})]
-    (if-not (<= 200 (:status resp) 299)
-      (throw+ {:error_code ERR_REQUEST_FAILED
-               :body (:body resp)})
-      (:body resp))))
+  (let [user   (:username current-user)
+        url    (key-url user)
+        _      (log/debug "user-session: DELETE" url)
+        resp   (cl/delete url {:throw-exceptions false})
+        status (:status resp)]
+    (cond (= 404 (:status resp))      (success-response)
+          (<= 200 (:status resp) 299) (success-response)
+          :else                       (request-failed resp))))
