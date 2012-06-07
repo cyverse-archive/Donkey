77e919c9e8621df4251da7e4bf814d1a3120d2f2
Dennis Roberts <dennis@iplantcollaborative.org>
2012-06-07 15:24:56 -0700

    CORE-3245: updated the iplant-service-config release number

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
   :iplant-rpm {:summary "iPlant Discovery Environment Business Layer Services"
                :release 3
                :provides "donkey"
-               :dependencies ["iplant-service-config >= 0.1.0-4"]
+               :dependencies ["iplant-service-config >= 0.1.0-5"]
                :config-files ["log4j.properties"]
                :config-path "conf/main"}
   :uberjar-exclusions [#"BCKEY.SF"]
