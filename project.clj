35feb72223c073a30344879e03a5b26e55d1b946
Dennis Roberts <dennis@iplantcollaborative.org>
2012-07-06 17:04:19 -0700

    CORE-3342: switch to the latest version of lein-iplant-rpm

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject donkey "1.2.0-SNAPSHOT"
+(defproject donkey "1.3.0-SNAPSHOT"
   :description "Framework for hosting DiscoveryEnvironment metadata services."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
@@ -11,7 +11,7 @@
                  [ring/ring-jetty-adapter "1.0.1"]
                  [net.sf.json-lib/json-lib "2.4" :classifier "jdk15"]
                  [slingshot "0.10.1"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.2.1-SNAPSHOT"]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.3.0-SNAPSHOT"]
             [lein-ring "0.7.1"]
             [swank-clojure "1.4.2"]]
   :profiles {:dev {:resource-paths ["conf/test"]}}
@@ -21,7 +21,6 @@
          :init donkey.core/load-configuration
          :port 31325}
   :iplant-rpm {:summary "iPlant Discovery Environment Business Layer Services"
-               :release 6
                :provides "donkey"
                :dependencies ["iplant-service-config >= 0.1.0-5"]
                :config-files ["log4j.properties"]
