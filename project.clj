07fc8b7d067b29b28bbd16a8d4045cb121fe21bf
Tony Edgin <tedgin@iplantcollaborative.org>
2012-11-08 10:33:36 -0700

    Incremented version number to support deployment.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject donkey "1.3.0-SNAPSHOT"
+(defproject donkey "1.3.1-SNAPSHOT"
   :description "Framework for hosting DiscoveryEnvironment metadata services."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
