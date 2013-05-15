db978728c5b8b4b28383f1b81a2c3506e6bee879
Dennis Roberts <dennis@iplantcollaborative.org>
2013-05-15 14:55:07 -0700

    CORE-3491: some minor refactoring

--- src/donkey/services/file_listing.clj
+++ src/donkey/services/file_listing.clj
@@ -8,14 +8,6 @@
             [clojure.string :as string]
             [clojure.tools.logging :as log]))
 
-(defn- build-path
-  "Builds a path from a base path and a list of components."
-  [path & components]
-  (string/join
-   "/"
-   (cons (string/replace path #"/+$" "")
-         (map #(string/replace % #"^/+|/+$" "") components))))
-
 (defn- save-default-output-dir
   "Saves the path to the user's default output folder in the user's prefs."
   [path]
@@ -23,26 +15,12 @@
    (cheshire/encode (assoc (cheshire/decode (user-prefs) true)
                       :defaultOutputFolder path))))
 
-(defn- get-or-create-dir
-  "Returns the path argument if the path exists and refers to a directory.  If
-   the path exists and refers to a regular file then nil is returned.
-   Otherwise, a new directory is created and the path is returned."
-  [path]
-  (log/debug "getting or creating dir: path =" path)
-  (let [stats (stat path)]
-    (cond (nil? stats)            (create path)
-          (= (:type stats) "dir") path
-          :else                   nil)))
-
 (defn- generate-output-dir
   "Automatically generates the default output directory based on the default
    name sent to the service."
   [base]
   (log/debug "generating output directory: base =" base)
-  (let [home (home-dir)
-        path (first
-              (filter #(not (nil? (get-or-create-dir %)))
-                      (cons base (map #(str base "-" %) (iterate inc 1)))))]
+  (let [path (gen-output-dir base)]
     (save-default-output-dir path)
     path))
 
