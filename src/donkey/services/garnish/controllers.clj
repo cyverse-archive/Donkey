25d76aac32c9a3bc5718584086577c49afe527a5
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-05-17 13:04:23 -0700

    Fixed up the controllers in garnish.

--- src/donkey/services/garnish/controllers.clj
+++ src/donkey/services/garnish/controllers.clj
@@ -1,10 +1,12 @@
 (ns donkey.services.garnish.controllers
   (:use [slingshot.slingshot :only [try+ throw+]]
-        [clojure-commons.error-codes])
+        [clojure-commons.error-codes]
+        [donkey.util.transformers :only [add-current-user-to-map]])
   (:require [cheshire.core :as json]
             [hoot.rdf :as rdf]
             [hoot.csv :as csv]
             [clojure.core.memoize :as memo]
+            [clojure.tools.logging :as log]
             [donkey.services.garnish.irods :as prods]))
 
 (defn check-missing-params
@@ -17,10 +19,10 @@
 (defn parse-body
   [body]
   (try+
-    (json/parse-string body :true)
+    (json/parse-string body true)
     (catch Exception e
       (throw+ {:error_code ERR_INVALID_JSON
-               :message e}))))
+               :message (str e)}))))
 
 (defn check-params-valid
   [params func-map]
@@ -40,45 +42,52 @@
   (set (concat rdf/accepted-languages csv/csv-types)))
 
 (defn add-type
-  [req-body params]
-  (let [body (parse-body req-body)]
+  [req-body req-params]
+  (let [body   (parse-body (slurp req-body))
+        params (add-current-user-to-map req-params)]
     (validate-params params {:user string?})
     (validate-params body {:path string? :type #(contains? (accepted-types) %)})
     (json/generate-string
       (prods/add-type (:user params) (:path body) (:type body)))))
 
 (defn delete-type
-  [params]
-  (validate-params params {:user string? :type #(contains? (accepted-types) %) :path string?})
-  (json/generate-string
-    (prods/delete-type (:user params) (:path params) (:type params))))
+  [req-params]
+  (let [params (add-current-user-to-map req-params)] 
+    (validate-params params {:user string? :type #(contains? (accepted-types) %) :path string?})
+    (json/generate-string
+      (prods/delete-type (:user params) (:path params) (:type params)))))
 
 (defn get-types
-  [params]
-  (validate-params params {:path string? :user string?})
-  (json/generate-string
-    {:types (prods/get-types (:user params) (:path params))}))
+  [req-params]
+  (let [params (add-current-user-to-map req-params)] 
+    (validate-params params {:path string? :user string?})
+    (json/generate-string
+      {:types (prods/get-types (:user params) (:path params))})))
 
 (defn find-typed-paths
-  [params]
-  (validate-params params {:user string? :type string?})
-  (json/generate-string
-    {:paths (prods/find-paths-with-type (:user params) (:path params))}))
+  [req-params]
+  (let [params (add-current-user-to-map req-params)] 
+    (validate-params params {:user string? :type string?})
+    (json/generate-string
+      {:paths (prods/find-paths-with-type (:user params) (:path params))})))
 
 (defn get-type-list 
   [] 
   (json/generate-string {:types csv/csv-types}))
 
 (defn set-auto-type
-  [req-body params]
-  (let [body (parse-body req-body)]
+  [req-body req-params]
+  (let [body   (parse-body (slurp req-body))
+        params (add-current-user-to-map req-params)]
+    (log/warn body)
     (validate-params params {:user string?})
     (validate-params body {:path string?})
     (json/generate-string
       (prods/auto-add-type (:user params) (:path body)))))
 
 (defn preview-auto-type
-  [params]
-  (validate-params params {:user string? :path string?})
-  (json/generate-string
-    (prods/preview-auto-type (:user params) (:path params))))
+  [req-params]
+  (let [params (add-current-user-to-map req-params)] 
+    (validate-params params {:user string? :path string?})
+    (json/generate-string
+      (prods/preview-auto-type (:user params) (:path params)))))
