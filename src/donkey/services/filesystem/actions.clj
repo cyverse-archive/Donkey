81283cc4b0d9fe219a9d6efd9f6238842c44dcf9
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-09-25 07:51:06 -0700

    Fixed some indentation issues.

--- src/donkey/services/filesystem/actions.clj
+++ src/donkey/services/filesystem/actions.clj
@@ -261,16 +261,16 @@
      cm [user]
      (format-call "create" user path)
      (let [fixed-path (ft/rm-last-slash path)]
-             (when-not (good-string? fixed-path)
-               (throw+ {:error_code ERR_BAD_OR_MISSING_FIELD
-                        :path path}))
-             (validators/user-exists cm user)
-             (validators/path-writeable cm user (ft/dirname fixed-path))
-             (validators/path-not-exists cm fixed-path)
-
-             (mkdir cm fixed-path)
-             (set-owner cm fixed-path user)
-             {:path fixed-path :permissions (collection-perm-map cm user fixed-path)}))))
+       (when-not (good-string? fixed-path)
+         (throw+ {:error_code ERR_BAD_OR_MISSING_FIELD
+                  :path path}))
+       (validators/user-exists cm user)
+       (validators/path-writeable cm user (ft/dirname fixed-path))
+       (validators/path-not-exists cm fixed-path)
+       
+       (mkdir cm fixed-path)
+       (set-owner cm fixed-path user)
+       {:path fixed-path :permissions (collection-perm-map cm user fixed-path)}))))
 
 (defn source->dest
   [source-path dest-path]
