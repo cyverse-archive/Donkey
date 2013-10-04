1a7e2ce903893e45079152eba4e1e5f85a9d86a8
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-10-04 12:41:43 -0700

    Added logging messages when the iRODS-related messaging is enabled or disabled.

--- src/donkey/util/messaging.clj
+++ src/donkey/util/messaging.clj
@@ -1,6 +1,7 @@
 (ns donkey.util.messaging
   (:require [donkey.services.garnish.messages :as ftype]
             [donkey.clients.amqp :as amqp]
+            [donkey.util.config :as cfg]
             [clojure.tools.logging :as log]))
 
 (defn dataobject-added
@@ -17,11 +18,18 @@
    The payload is passed to handlers as a byte stream. That should theoretically give us the
    ability to handle binary data arriving in messages, even though that doesn't seem likely."
   [channel {:keys [routing-key content-type delivery-tag type] :as meta} ^bytes payload]
+  (log/warn (format "[message-handler] [%s] [%s]" routing-key (String. payload "UTF-8")))
   (case routing-key
     "data-object.added" (dataobject-added payload)
-    (log/warn (format "[message-handler] [%s] %s" routing-key (String. payload "UTF-8")))))
+    nil))
 
 (defn messaging-initialization
   "Initializes the AMQP messaging handling, registering (message-handler) as the callback."
   []
-  (amqp/configure message-handler))
+  (if-not (cfg/rabbitmq-enabled)
+    (log/warn "[messaging-initialization] iRODS messaging disabled"))
+  
+  (when (cfg/rabbitmq-enabled)
+    (log/warn "[messaging-initialization] iRODS messaging enabled")
+    (amqp/configure message-handler)
+    (amqp/conn-monitor message-handler)))
