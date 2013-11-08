3f2009de204799c7a96d314b16a5b6fceb8b604d
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-11-08 15:08:46 -0700

    Switched the read-csv-chunk endpoint to use pages.
    Callers can pass in a page that they want to jump to and the API will
    calculate the correct position to start reading from.

--- src/donkey/services/filesystem/page_csv.clj
+++ src/donkey/services/filesystem/page_csv.clj
@@ -64,46 +64,84 @@
   [start-pos trimmed-chunk]
   (+ start-pos (- (count (.getBytes trimmed-chunk)) 1)))
 
+(defn- fix-record
+  [record]
+  (into {} 
+    (map (fn [[k v]] (if (nil? v) [k ""] [k v])) 
+         (seq record))))
+
 (defn- read-csv
   [separator csv-str]
   (if-not (string/blank? csv-str)
     (let [ba  (java.io.ByteArrayInputStream. (.getBytes csv-str))
           isr (java.io.InputStreamReader. ba "UTF-8")]
-      (mapv #(zipmap (mapv str (range (count %1))) %1) 
-            (mapv vec (.readAll (CSVReader. isr (.charAt separator 0))))))
+      (map fix-record (mapv #(zipmap (mapv str (range (count %1))) %1) 
+                           (mapv vec (.readAll (CSVReader. isr (.charAt separator 0)))))))
     [{}]))
+
+(defn num-pages
+  [chunk-size file-size]
+  (int (Math/ceil (double (/ file-size chunk-size)))))
+
+(defn start-pos
+  [page chunk-size]
+  (* chunk-size (- page 1)))
 
 (defn- read-csv-chunk
   "Reads a chunk of a file and parses it as a CSV. The position and chunk-size are not guaranteed, since
    we shouldn't try to parse partial rows. We scan forward from the starting position to find the first
    line-ending and then scan backwards from the last position for the last line-ending."
-  [user path position chunk-size separator]
+  [user path page chunk-size separator]
   (with-jargon (jargon-cfg) [cm]
-    (log/warn "[read-csv-chunk]" user path position chunk-size separator)
+    (log/warn "[read-csv-chunk]" user path page chunk-size separator)
     (validators/user-exists cm user)
     (validators/path-exists cm path)
     (validators/path-is-file cm path)
     (validators/path-readable cm user path)
-    (let [chunk         (read-at-position cm path position chunk-size)
-          front-trimmed (trim-front position chunk)
-          new-start-pos (calc-start-pos position chunk front-trimmed)
-          trimmed-chunk (trim-back path position (file-size cm path) chunk front-trimmed)
-          new-end-pos   (calc-end-pos position trimmed-chunk)
-          the-csv       (read-csv separator trimmed-chunk)]
-      {:path       path
-       :user       user
-       :max-cols   (str (reduce #(if (>= %1 %2) %1 %2) (map count the-csv)))
-       :start      (str new-start-pos)
-       :end        (str new-end-pos)
-       :chunk-size (str (count (.getBytes trimmed-chunk)))
-       :file-size  (str (file-size cm path))
-       :csv        the-csv})))
+    (let [page     (Integer/parseInt page)
+          fsize    (file-size cm path)
+          pages    (num-pages chunk-size fsize)
+          position (start-pos page chunk-size)]
+      
+      (when-not (<= page pages)
+        (throw+ {:error_code   "ERR_INVALID_PAGE"
+                 :page         (str page)
+                 :number-pages (str pages)}))
+      
+      (let [chunk         (read-at-position cm path position chunk-size)
+            _             (log/warn chunk)
+            front-trimmed (trim-front position chunk)
+            new-start-pos (calc-start-pos position chunk front-trimmed)
+            trimmed-chunk (trim-back path position (file-size cm path) chunk front-trimmed)
+            _             (log/warn trimmed-chunk)
+            new-end-pos   (calc-end-pos position trimmed-chunk)
+            the-csv       (read-csv separator trimmed-chunk)
+            _             (log/warn the-csv)]
+        {:path         path
+         :number-pages (str pages)
+         :user         user
+         :max-cols     (str (reduce #(if (>= %1 %2) %1 %2) (map count the-csv)))
+         :start        (str new-start-pos)
+         :end          (str new-end-pos)
+         :chunk-size   (str (count (.getBytes trimmed-chunk)))
+         :file-size    (str (file-size cm path))
+         :csv          the-csv}))))
+
+(defn integer-string?
+  [str-to-check]
+  (try
+    (Integer/parseInt str-to-check)
+    true
+    (catch Exception e false)))
 
 (with-pre-hook! #'read-csv-chunk
-  (fn [user path position chunk-size separator]
-    (when-not (>= position 0)
-      (throw+ {:error_code "ERR_POSITION_NOT_POS"
-               :position   (str position)}))
+  (fn [user path page chunk-size separator]
+    (when-not (integer-string? page)
+      (throw+ {:error_code "ERR_PAGE_NOT_INT"
+               :page       page}))
+    (when-not (>= (Integer/parseInt page) 0)
+      (throw+ {:error_code "ERR_PAGE_NOT_POS"
+               :page       page}))
     (when-not (pos? chunk-size)
       (throw+ {:error_code "ERR_CHUNK_TOO_SMALL"
                :chunk-size (str chunk-size)}))))
@@ -111,20 +149,23 @@
 (defn do-read-csv-chunk
   [{user :user} 
    {path :path 
-    separator :separator 
-    position :position 
+    separator  :separator 
+    page       :page
     chunk-size :chunk-size}]
-  (let [pos    (Long/parseLong position)
-        size   (Long/parseLong chunk-size)]
-    (read-csv-chunk user path pos size (url/url-decode separator))))
+  (let [size (Long/parseLong chunk-size)]
+    (read-csv-chunk user path page size (url/url-decode separator))))
 
 (with-pre-hook! #'do-read-csv-chunk
   (fn [params body]
     (log/warn "[call][do-read-csv-chunk]" params body)
     (validate-map params {:user string?})
-    (validate-map body {:path        string? 
-                        :position    string? 
-                        :chunk-size  string? 
+    (validate-map body {:path        string?
+                        :page        string?
+                        :chunk-size  string?
                         :separator   string?})))
 
 (with-post-hook! #'do-read-csv-chunk (log-func "do-read-csv-chunk"))
+
+;;; Make sure that page exists
+;;; Make sure that integer
+;;; TODO: Make sure that page is an integer
