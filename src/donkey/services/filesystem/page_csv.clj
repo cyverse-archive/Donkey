de337d7ec66cbcb99e2a0197b77f812a11f45960
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-11-12 14:00:13 -0700

    Fixed the CSV paging. Maybe. Hopefully.
    We're now reading in 2 pages for every page requested. We go back
    byte-by-byte from the start of the requested page until we hit a
    newline and trim the chunk from that point. We do the same for the end
    of the chunk. This should give us consistent lines.

--- src/donkey/services/filesystem/page_csv.clj
+++ src/donkey/services/filesystem/page_csv.clj
@@ -1,5 +1,5 @@
 (ns donkey.services.filesystem.page-csv
-  (:use [clojure-commons.error-codes] 
+  (:use [clojure-commons.error-codes]
         [donkey.util.config]
         [donkey.util.validators]
         [donkey.services.filesystem.common-paths]
@@ -20,62 +20,39 @@
 
 (def line-ending "\n")
 
-(defn- trim-to-line-start
-  [str-chunk]
-  (let [line-pos (+ (.indexOf str-chunk line-ending) 1)]
-    
-    (cond
-      (<= line-pos 1) 
-      str-chunk
-      
-      (>= line-pos (- (count str-chunk) 1))
-      ""
-      
-      :else
-      (.substring str-chunk line-pos))))
+(defn- chunk-start
+  [page chunk-size]
+  (if (= page 0)
+    0
+    (dec chunk-size)))
 
-(defn- trim-front
-  [position chunk]
-  (if-not (pos? position) 
-    chunk 
-    (trim-to-line-start chunk)))
+(defn- seek-prev-line
+  [chunk start-pos]
+  (loop [pos start-pos]
+    (let [start-str (nth chunk pos)]
+      (if-not (or (= pos 0)
+                  (= start-str (.charAt line-ending 0)))
+        (recur (dec pos))
+        (if (= pos 0) 0 (inc pos))))))
 
-(defn- calc-start-pos
-  "Calculates the new start position after (trim-to-line-start) has been called."
-  [start-pos orig-chunk trimmed-chunk]
-  (+ start-pos (- (count (.getBytes orig-chunk)) (count (.getBytes trimmed-chunk)))))
-
-(defn- trim-to-last-line
-  [str-chunk]
-  (let [calced-pos (log/spy (- (.lastIndexOf str-chunk line-ending) 1))
-        last-pos   (log/spy (if-not (pos? calced-pos) 0 calced-pos))]
-    (.substring str-chunk 0 last-pos)))
-
-(defn- trim-back
-  [path position file-size chunk front-trimmed-chunk]
-  (log/warn front-trimmed-chunk)
-  (if (>= (+ position (- (count (.getBytes chunk)) 1)) 
-          (- file-size 1)) 
-    front-trimmed-chunk
-    (log/spy (trim-to-last-line front-trimmed-chunk))))
-
-(defn- calc-end-pos
-  "Calculates the new ending byte based on the start position and the current size of the chunk."
-  [start-pos trimmed-chunk]
-  (+ start-pos (- (count (.getBytes trimmed-chunk)) 1)))
+(defn- trim-chunk
+  [chunk page chunk-size]
+  (let [lstart (seek-prev-line chunk (chunk-start page chunk-size))
+        lend   (seek-prev-line chunk (dec (.length chunk)))]
+    (.substring chunk lstart lend)))
 
 (defn- fix-record
   [record]
-  (into {} 
-    (map (fn [[k v]] (if (nil? v) [k ""] [k v])) 
+  (into {}
+    (map (fn [[k v]] (if (nil? v) [k ""] [k v]))
          (seq record))))
 
 (defn- read-csv
-  [separator csv-str]
+  [csv-str separator]
   (if-not (string/blank? csv-str)
     (let [ba  (java.io.ByteArrayInputStream. (.getBytes csv-str))
           isr (java.io.InputStreamReader. ba "UTF-8")]
-      (map fix-record (mapv #(zipmap (mapv str (range (count %1))) %1) 
+      (map fix-record (mapv #(zipmap (mapv str (range (count %1))) %1)
                            (mapv vec (.readAll (CSVReader. isr (.charAt separator 0)))))))
     [{}]))
 
@@ -85,7 +62,13 @@
 
 (defn start-pos
   [page chunk-size]
-  (* chunk-size (- page 1)))
+  (if (= page 0) 0 (* chunk-size page)))
+
+(defn- calc-chunk-size
+  [page chunk-size]
+  (if (= page 0)
+    chunk-size
+    (* chunk-size 2)))
 
 (defn- read-csv-chunk
   "Reads a chunk of a file and parses it as a CSV. The position and chunk-size are not guaranteed, since
@@ -98,33 +81,27 @@
     (validators/path-exists cm path)
     (validators/path-is-file cm path)
     (validators/path-readable cm user path)
-    (let [page     (Integer/parseInt page)
-          fsize    (file-size cm path)
-          pages    (num-pages chunk-size fsize)
-          position (start-pos page chunk-size)]
-      
+    (let [page            (dec (Integer/parseInt page))
+          start-pg        (if (= page 0) 0 (dec page))
+          full-chunk-size (calc-chunk-size page chunk-size)
+          fsize           (file-size cm path)
+          pages           (num-pages chunk-size fsize)
+          position        (start-pos page chunk-size)
+          load-pos        (start-pos start-pg chunk-size)]
+
       (when-not (<= page pages)
         (throw+ {:error_code   "ERR_INVALID_PAGE"
                  :page         (str page)
                  :number-pages (str pages)}))
-      
-      (let [chunk         (read-at-position cm path position chunk-size)
-            _             (log/warn chunk)
-            front-trimmed (trim-front position chunk)
-            new-start-pos (calc-start-pos position chunk front-trimmed)
-            trimmed-chunk (trim-back path position (file-size cm path) chunk front-trimmed)
-            _             (log/warn trimmed-chunk)
-            new-end-pos   (calc-end-pos position trimmed-chunk)
-            the-csv       (read-csv separator trimmed-chunk)
-            _             (log/warn the-csv)]
+
+      (let [chunk   (trim-chunk (read-at-position cm path load-pos full-chunk-size) page chunk-size)
+            the-csv (read-csv chunk separator)]
         {:path         path
          :number-pages (str pages)
          :user         user
          :max-cols     (str (reduce #(if (>= %1 %2) %1 %2) (map count the-csv)))
-         :start        (str new-start-pos)
-         :end          (str new-end-pos)
-         :chunk-size   (str (count (.getBytes trimmed-chunk)))
-         :file-size    (str (file-size cm path))
+         :chunk-size   (str (count (.getBytes chunk)))
+         :file-size    (str fsize)
          :csv          the-csv}))))
 
 (defn integer-string?
@@ -147,9 +124,9 @@
                :chunk-size (str chunk-size)}))))
 
 (defn do-read-csv-chunk
-  [{user :user} 
-   {path :path 
-    separator  :separator 
+  [{user :user}
+   {path :path
+    separator  :separator
     page       :page
     chunk-size :chunk-size}]
   (let [size (Long/parseLong chunk-size)]
@@ -164,7 +141,9 @@
                         :chunk-size  string?
                         :separator   string?})))
 
-(with-post-hook! #'do-read-csv-chunk (log-func "do-read-csv-chunk"))
+(with-post-hook! #'do-read-csv-chunk
+  (fn [result]
+    (log/warn "[result][do-read-csv-chunk]") (dissoc result :csv)))
 
 ;;; Make sure that page exists
 ;;; Make sure that integer
