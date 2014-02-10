baac3327ad2ef403bb6ef48a8edfb46ed13c4280
John Wregglesworth <wregglej@iplantcollaborative.org>
2014-02-10 15:41:32 -0700

    CORE-5160: Allow deletions by attr and value.

--- src/donkey/services/filesystem/metadata.clj
+++ src/donkey/services/filesystem/metadata.clj
@@ -52,7 +52,7 @@
     (log/warn "check" (true? (attr-value? cm fixed-path attr value)))
     (when-not (attr-value? cm fixed-path attr value)
       (log/warn "Adding " attr value "to" fixed-path)
-      (set-metadata cm fixed-path attr value new-unit))
+      (add-metadata cm fixed-path attr value new-unit))
     fixed-path))
 
 (defn metadata-set
@@ -84,9 +84,11 @@
    in an AVU is formatted a certain way, it can't be deleted.
    We're base64 encoding the value before deletion to ensure
    that the deletion will work."
-  [cm path attr]
-  (let [{:keys [attr value unit]} (first (get-attribute cm path attr))]
-    (set-metadata cm path attr (encode-str value) unit)))
+  [cm path attr value]
+  (let [{:keys [attr value unit]} (first (get-attribute-value cm path attr value))
+        new-val (encode-str value)]
+    (add-metadata cm path attr new-val unit)
+    new-val))
 
 (defn- metadata-batch-set
   [user path adds-dels]
@@ -96,27 +98,25 @@
     (validators/path-writeable cm user path)
     (let [new-path (ft/rm-last-slash path)]
       (doseq [del (:delete adds-dels)]
-        (when (attribute? cm new-path del)
-          (workaround-delete cm new-path del)
-          (delete-metadata cm new-path del)))
+        (let [attr  (:attr del)
+              value (:value del)]
+          (if (attr-value? cm new-path attr value)
+            (delete-metadata cm new-path attr value))))
       (doseq [avu (:add adds-dels)]
         (let [new-unit (reserved-unit avu)
               attr     (:attr avu)
               value    (:value avu)]
           (if-not (attr-value? cm new-path attr value)
-            (set-metadata cm new-path attr value new-unit))))
+            (add-metadata cm new-path attr value new-unit))))
       {:path new-path :user user})))
 
 (defn metadata-delete
-  [user path attr]
+  [user path attr value]
   (with-jargon (jargon-cfg) [cm]
     (validators/user-exists cm user)
     (validators/path-exists cm path)
     (validators/path-writeable cm user path)
-    (let [fix-unit #(if (= (:unit %1) IPCRESERVED) (assoc %1 :unit "") %1)
-          avu      (map fix-unit (get-metadata cm (ft/rm-last-slash path)))]
-      (workaround-delete cm path attr)
-      (delete-metadata cm path attr))
+    (delete-metadata cm path attr value)
     {:path path :user user}))
 
 (defn- check-adds
@@ -125,7 +125,7 @@
 
 (defn- check-dels
   [dels]
-  (mapv string? dels))
+  (mapv #(= (set (keys %)) (set [:attr :value :unit])) dels))
 
 (defn do-metadata-get
   [{user :user path :path}]
@@ -163,22 +163,26 @@
           path (:path params)
           adds (:add body)
           dels (:delete body)]
+      (log/warn (jargon-cfg))
       (when (pos? (count adds))
         (if (not (every? true? (check-adds adds)))
           (throw+ {:error_code ERR_BAD_OR_MISSING_FIELD :field "add"})))
       (when (pos? (count dels))
         (if-not (every? true? (check-dels dels))
-          (throw+ {:error_code ERR_BAD_OR_MISSING_FIELD :field "add"}))))))
+          (throw+ {:error_code ERR_BAD_OR_MISSING_FIELD :field "delete"}))))))
 
 (with-post-hook! #'do-metadata-batch-set (log-func "do-metadata-batch-set"))
 
 (defn do-metadata-delete
-  [{user :user path :path attr :attr}]
-  (metadata-delete user path attr))
+  [{user :user path :path attr :attr value :value}]
+  (metadata-delete user path attr value))
 
 (with-pre-hook! #'do-metadata-delete
   (fn [params]
     (log-call "do-metadata-delete" params)
-    (validate-map params {:user string? :path string? :attr string?})))
+    (validate-map params {:user string?
+                          :path string?
+                          :attr string?
+                          :value string?})))
 
 (with-post-hook! #'do-metadata-delete (log-func "do-metadata-delete"))
